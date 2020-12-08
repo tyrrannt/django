0e82d3ffd14921c71aef42af5b69401eeff6c476
tyrrannt <62670631+tyrrannt@users.noreply.github.com>
2020-12-08 18:03:52 +0300

    добавлены модели категорий и продуктов, и создан диспетчер URL

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -2,11 +2,18 @@
 
 from django.urls import path
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 from mainapp import views as mainapp_views
+from django.conf.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp_views.main, name='index'),
-    path('products/', mainapp_views.products, name='products'),
+    path('contact/', mainapp_views.contact, name='contact'),
+    path('products/', include('mainapp.urls', namespace='products')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
