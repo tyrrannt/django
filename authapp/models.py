2366c94ce5443dcb4c6723add438d129419778cd
tyrrannt <62670631+tyrrannt@users.noreply.github.com>
2020-12-11 12:34:54 +0300

    изменено условие определения статуса суперпользователя, и добавлено поле ДР для пользователя

--- authapp/models.py
+++ authapp/models.py
@@ -5,3 +5,4 @@ from django.contrib.auth.models import AbstractUser
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
     age = models.PositiveIntegerField(verbose_name='возраст', blank=True, null=True)
+    birthday = models.DateField(verbose_name='День рождения', blank=True, null=True)
