36e658024dad609cd7317acecb0bac46c45aff3f
tyrrannt <62670631+tyrrannt@users.noreply.github.com>
2020-12-26 12:59:13 +0300

    Внесены корректировки в шаблон, диспетчер URL и контроллер - приложения mainapp

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,8 +5,8 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp_views.products, name='index'),
-    path('<int:category_pk>/', mainapp_views.products, name='category'),
-    path('<int:category_pk>/<int:page>/<int:product_id>/', mainapp_views.product_item, name='product'),
-    path('<int:category_pk>/<int:page>/', mainapp_views.products, name='page'),
+    path('category/<int:pk>/', mainapp_views.products, name='category'),
+    path('category/<int:pk>/page/<int:page>/', mainapp_views.products, name='page'),
+    path('product/<int:pk>/', mainapp_views.product_item, name='product'),
 
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -11,11 +11,13 @@ def main(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def product_item(request, category_pk=None, page=1, product_id=None):
+def product_item(request, pk=None):
     basket = []
+    print(pk)
+
     title = 'GeekShop - Каталог'
     categories = ProductCategory.objects.all()
-    product = Product.objects.get(uid=product_id)
+    product = Product.objects.get(pk=pk)
     if request.user.is_authenticated:
         basket = Basket.objects.filter(user=request.user)
     context = {
@@ -23,29 +25,30 @@ def product_item(request, category_pk=None, page=1, product_id=None):
         'categories': categories,
         'title': title,
         'product': product,
-        'pk': category_pk,
     }
     return render(request, 'mainapp/product.html', context)
 
 
-def products(request, category_pk=0, page=1, product_id=None):
+def products(request, pk=None, page=1):
     basket = []
     title = 'GeekShop - Каталог'
-    categories = ProductCategory.objects.all()
+    categories = ProductCategory.objects.filter(is_active=True)
     if request.user.is_authenticated:
         basket = Basket.objects.filter(user=request.user)
-    if category_pk:
-        if category_pk == '0':
+    if pk is not None:
+        if pk == 0:
             products = Product.objects.all().order_by('price')
+            category = {'pk': 0}
         else:
-            products = Product.objects.filter(category=ProductCategory.objects.get(pk=category_pk)).order_by('price')
-            title = 'GeekShop - ' + ProductCategory.objects.get(pk=category_pk).name
+            products = Product.objects.filter(category=ProductCategory.objects.get(pk=pk)).order_by('price')
+            category = get_object_or_404(ProductCategory, pk=pk)
+            title = 'GeekShop - ' + ProductCategory.objects.get(pk=pk).name
         context = {
             'title': title,
             'categories': categories,
             'products': products,
             'baskets': basket,
-            'category_pk': category_pk,
+            'category': category,
         }
         paginator = Paginator(products, 3)
         try:
@@ -56,24 +59,27 @@ def products(request, category_pk=0, page=1, product_id=None):
             products_paginator = paginator.page(paginator.num_pages)
         context.update({'products': products_paginator})
         return render(request, 'mainapp/products.html', context)
-    else:
-        products = Product.objects.all().order_by('price')
-        context = {
-            'title': title,
-            'categories': categories,
-            'products': products,
-            'baskets': basket,
-            'category_pk': category_pk,
-        }
-        paginator = Paginator(products, 3)
-        try:
-            products_paginator = paginator.page(page)
-        except PageNotAnInteger:
-            products_paginator = paginator.page(1)
-        except EmptyPage:
-            products_paginator = paginator.page(paginator.num_pages)
-        context.update({'products': products_paginator})
-        return render(request, 'mainapp/products.html', context)
+
+    category = {
+        'pk': 0,
+    }
+    products = Product.objects.all().order_by('price')
+    context = {
+        'title': title,
+        'categories': categories,
+        'products': products,
+        'baskets': basket,
+        'category': category,
+    }
+    paginator = Paginator(products, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context.update({'products': products_paginator})
+    return render(request, 'mainapp/products.html', context)
 
 
 def contact(request):
